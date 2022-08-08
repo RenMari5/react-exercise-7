b03ed08eb1e86f6212f3a68a548775d3b6824ee3
Ren <rpaczkowski@gmail.com>
2022-08-08 13:20:03 -0400

    Created all folders and documents

---
+++ react-exercise-7/src/types.ts
@@ -0,0 +1,10 @@
+import { NumberLiteralType } from "typescript";
+
+export interface Item {
+  id: string;
+  name: string;
+  description: string;
+  calories: number;
+  price: number;
+  vegetarian: boolean;
+}
